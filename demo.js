e8cdee8c04672ddf42cd60eef8b84be38683e2fd
stephanieLQ <52709869+stephanieLQ@users.noreply.github.com>
2022-09-02 22:22:02 -0400

    modificacion el archivo demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-console.log("Hola clase dia 6 - 25/08/2022");
-=======
-console.log("Hola clase dia 6 - Agosto 25, 2022");
->>>>>>> dia6
+console.log("Buenos noches clase dia 6 - 25/08/2022");
 console.log("Diplomado Fullstack");
